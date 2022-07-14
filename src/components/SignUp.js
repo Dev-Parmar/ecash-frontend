60f4e37bd37995f208abebb5d8a273e98e1cd598
Dev <dparmar1@isi-montreal.com>
2022-07-13 22:24:01 -0400

    Adding signup data into database using api

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,6 +1,7 @@
 import { Button, Container, Grid, TextField, Typography } from '@mui/material'
 
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
 
@@ -12,6 +13,7 @@ const SignUp = () => {
     const [emailErr, setEmailErr] = useState(false)
     const [passwordErr, setPasswordErr] = useState(false)
 
+    const navigate = useNavigate()
 
     const sty = () => {
         return ({
@@ -20,28 +22,34 @@ const SignUp = () => {
         })
     }
 
-    const handleSubmit = () => {
-        if (name) {
-            setNameErr(false)
-        } else {
-            setNameErr(true)
-        }
+    const handleSubmit = async () => {
 
-        if (email) {
-            setEmailErr(false)
-        } else {
-            setEmailErr(true)
-        }
+        name ? setNameErr(false) : setNameErr(true)
+        email ? setEmailErr(false) : setEmailErr(true)
+        password ? setPasswordErr(false) : setPasswordErr(true)
 
-        if (password) {
-            setPasswordErr(false)
-        } else {
-            setPasswordErr(true)
-        }
 
-        console.log(name, email, password)
+        if (name && email && password) {
+            console.log("go")
+
+
+            let data = await fetch('http://127.0.0.1:6969/register', {
+                method: 'post',
+                body: JSON.stringify({ name, email, password }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            })
+            let final = await data.json()
+            if (final) {
+                navigate('/')
+            } else {
+                console.log('Error signing up')
+            }
+        } else {
+            console.log('Please fill the fields')
+        }
     }
-
 
 
     return (
@@ -50,13 +58,13 @@ const SignUp = () => {
                 <Typography variant='h3' sx={{ padding: '0.5em' }}>Register</Typography>
                 <Grid container direction={'column'} >
                     <Grid item >
-                        <TextField variant='outlined' error={nameErr} type={'text'} value={name} onChange={(e) => { setName(e.target.value) }} label='Name' sx={sty} required />
+                        <TextField variant='outlined' error={nameErr} type={'text'} value={name} onChange={(e) => { setName(e.target.value); if (e.target.value === '' || e.target.value === null) { setNameErr(true) } else { setNameErr(false) } }} label='Name' sx={sty} required />
                     </Grid>
                     <Grid item >
-                        <TextField variant='outlined' error={emailErr} type={'email'} value={email} onChange={(e) => { setEmail(e.target.value) }} label='Email' sx={sty} required />
+                        <TextField variant='outlined' error={emailErr} type={'email'} value={email} onChange={(e) => { setEmail(e.target.value); if (e.target.value === '' || e.target.value === null) { setEmailErr(true) } else { setEmailErr(false) } }} label='Email' sx={sty} required />
                     </Grid>
                     <Grid item >
-                        <TextField variant='outlined' error={passwordErr} type={'password'} value={password} onChange={(e) => { setPassword(e.target.value) }} label='Password' sx={sty} required />
+                        <TextField variant='outlined' error={passwordErr} type={'password'} value={password} onChange={(e) => { setPassword(e.target.value); if (e.target.value === '' || e.target.value === null) { setPasswordErr(true) } else { setPasswordErr(false) } }} label='Password' sx={sty} required />
                     </Grid>
                     <Grid item>
                         <Button size='large' variant='contained' onClick={handleSubmit} sx={{ margin: '1em' }}>Submit</Button>
