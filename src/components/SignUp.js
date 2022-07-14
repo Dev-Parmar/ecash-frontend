36852f7b472c694f83b010a92e3971593e6b2e13
Dev <dparmar1@isi-montreal.com>
2022-07-13 20:32:13 -0400

    Footer and SignUp form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, Input, InputLabel, TextField, Typography } from '@mui/material'
+import { Button, Container, Grid, TextField, Typography } from '@mui/material'
 
 import React, { useState } from 'react'
 
